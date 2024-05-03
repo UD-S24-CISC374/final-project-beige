0f53556dba63cf990f84d8d9451e484ed05f905e
uytyks <uytyks@udel.edu>
2024-05-02 22:27:41 -0400

    wizard56 renamed and impoortant.txt reads better now

--- src/fs/CatFS.ts
+++ src/fs/CatFS.ts
@@ -238,14 +238,14 @@ cat FILE: Read a FILE that you choose in the terminal!
 
 ls: Lists everything in your current directory!`,
 
-    "/home/logs/log4-15-2024.txt": `wizard56: Can we really do this? That's awesome!
+    "/home/logs/log4-15-2024.txt": `blonk291: Can we really do this? That's awesome!
 lizard58: Of course we can, we'll just need a name.
 lizard58: There's no way we can get caught.
-wizard56: But we need justice for our friend.
+blonk291: But we need justice for our friend.
 lizard58: Exactly. I've got just the name for us.
-wizard56: Wait hold on who let you pick the name?
+blonk291: Wait hold on who let you pick the name?
 lizard58: Me, I'm literally the guy who has coding experience here dummy!!
-wizard56: Man I wanna pick the name, Im great at naming things.
+blonk291: Man I wanna pick the name, Im great at naming things.
 lizard58: No way am I letting you name our alias.
 lizard58: You'll name us a number or something stupid.`,
 
@@ -259,25 +259,25 @@ You ever encounter a directory that ends in .zip? With no way to open it?
 Well girl do I have a command for you!
 INTRODUCING "unzip DIRECTORY.zip" This will unzip that directory so you can get to explorin it!`,
 
-    "/home/logs/dir2.zip/rm.txt": `wizard56: Duuuuuuuuuuuuuuuude I accidentally made a virus.
+    "/home/logs/dir2.zip/rm.txt": `blonk291: Duuuuuuuuuuuuuuuude I accidentally made a virus.
 lizard58: HOW DO YOU JUST *MAKE* A VIRUS???
-wizard56: Idk dude but I need to remove it STAT.
+blonk291: Idk dude but I need to remove it STAT.
 lizard58: Ohhhhhhhhh boy ok. Use the rm command.
-wizard56: rm? That stand for remove?
+blonk291: rm? That stand for remove?
 lizard58: YES YOU TYPE IN rm AND THEN WHAT YOU WANT TO REMOVE
-wizard56: So like "rm virus"?
+blonk291: So like "rm virus"?
 lizard58: IF YOU ACTUALLY NAMED YOUR VIRUS virus THEN YES. YES.
-wizard56: Thank you :)
+blonk291: Thank you :)
 lizard58: I'm going to rm you in a second.`,
 
     "/home/logs/dir2.zip/log4-20-2024.txt": `lizard58: Alright it's done!
-wizard56: YES! THEY'LL NEVER KNOW WHO HIT EM!
+blonk291: YES! THEY'LL NEVER KNOW WHO HIT EM!
 lizard58: The articles will be CRAZY after this one.
 lizard58: We should lock them up in case our logs get out.
-wizard56: Ooh ooh! Ok I'll make a program that can lock it!
+blonk291: Ooh ooh! Ok I'll make a program that can lock it!
 lizard58: Ok I'll leave it to you, we just have to hope no one gets rid of it.
-wizard56: Nah that would never happen, I'll hide it somewhere far from here.
-wizard56: It's in a different directory, one you might not have seen yet.`,
+blonk291: Nah that would never happen, I'll hide it somewhere far from here.
+blonk291: It's in a different directory, one you might not have seen yet.`,
 
     "/project/1100/cat.zip/cat/find_me.txt": `If you see this, please find me.
 
@@ -314,13 +314,13 @@ like... a cyber angel, destined to flitter around on unseen wings in front of th
 They will regret putting me in here.`,
 
     "/project/trash/important.txt": `Hey you! yeah you!
-I thought this file would be fun to write so here you are
+I thought this would be fun to write so here you are
 Guess it paid off since you're reading it!
 Here i can write whatever i want for you :)
 Real talk though, you need a password.
 I think i can help you with that.
-See this text? The password is hidden all within these lines.
-Enter it to gain access to the 0011 directory! good luck!`,
+See this text? The password is these lines.
+Enter it to gain access to the 0011 directory!`,
 
     "/project/1100/entry3PARTA.txt": `The good part about being in a computer is that everything makes sense. 
 Everything is just data. I'M just data. 
